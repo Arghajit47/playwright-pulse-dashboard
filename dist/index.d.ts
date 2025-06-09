6640bf129595a6d34ccd549de3b175d5ecf547a7
Personal Postman Reporter <personalpostmanreporter@gmail.com>
2025-06-09 12:54:59 +0000

    Found 195 errors in 47 files.
    Errors  Files
        27  src/components/puls

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { PulseDashboard } from './components/pulse-dashboard/PulseDashboard';
-export type { PlaywrightPulseReport, HistoricalTrend, DetailedTestResult, RunMetadata, TestStep, FlakyTestDetail, FlakyTestOccurrence, ScreenshotAttachment, } from './types/playwright';
-export type { TestStatusFilter } from './components/pulse-dashboard/LiveTestResults';
-//# sourceMappingURL=index.d.ts.map
