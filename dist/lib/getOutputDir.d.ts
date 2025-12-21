82db727d3c6d0da4833c83e2765deb57eed1896f
Arghajit Singha <arghajitsingha@MacBookAir.lan>
2025-12-21 07:44:06 +0530

    chore: remove dist directory files and update README.md

---
+++ dist/lib/getOutputDir.d.ts
@@ -0,0 +1,3 @@
+#!/usr/bin/env node
+export declare function getOutputDir(customOutputDirFromArgs?: any): Promise<string>;
+//# sourceMappingURL=getOutputDir.d.ts.map
--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@
 import type {NextConfig} from 'next';
 
 const nextConfig = {
-  output: "standalone",
   typescript: {
     ignoreBuildErrors: true,
   },
